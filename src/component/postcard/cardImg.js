ca8aef0bbe471e4686c60768e571f5729c981d0e
王超 <chao198802@163.com>
2016-11-01 20:37:27 +0800

    wxapi

--- src/component/postcard/cardImg.js
+++ src/component/postcard/cardImg.js
@@ -3,12 +3,26 @@ import ReactDom from 'react-dom';
 
 import './cardImg.less'
 
+var wx = require('weixin-js-sdk');
+
 export default class CardImg extends React.Component{
 	state={
-		to:'孔明、张飞',
-		content:'生命无常，各奔天涯，独坐而嘤嘤。尤记绕柱而转，贴墙而遁,隔帘相扑。母上安好，勿忧，但缅过往。',
-		sender:'曹操',
-		address:'海角市天涯路1号'
+		to:'孔明、张飞'
+	}
+	
+	componentWillMount(){
+		wx.config({
+		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		    appId: '', // 必填，公众号的唯一标识
+		    timestamp: , // 必填，生成签名的时间戳
+		    nonceStr: '', // 必填，生成签名的随机串
+		    signature: '',// 必填，签名，见附录1
+		    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+		wx.ready(function(){
+			console.log('微信ready');
+		    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+		});
 	}
 	
 	render(){
@@ -18,5 +32,9 @@ export default class CardImg extends React.Component{
 
 			</div>
 		);
+	}
+	
+	click(){
+		//选择照片
 	}
 }
